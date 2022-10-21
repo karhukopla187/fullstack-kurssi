1daa2b88efdbf2e86aea7db76a9a28296df7561f
karhukopla187 <ohmanpatrick@gmail.com>
2022-10-21 07:55:08 +0300

    pe

---
+++ quiz/src/QuizCollection.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Quiz from './Quiz'
+
+const QuizCollection = (props) => {
+  return (
+    <>
+      <div>{props.quizCollection.name}</div>
+      <input type="text" onChange={event=>
+        { props.dispatch({type:"QC_NAME_CHANGED",
+          payload:{index:props.quizCollectionIndex,name:event.target.value} })}} 
+          value = {props.quizCollection.name}/>
+      <div>Luokat:</div>
+      <div>{props.quizCollection.quizList.map((quiz,index) => 
+        <Quiz dispatch={props.dispatch} quizCollectionIndex={props.QuizCollection} 
+          quizIndex={index} 
+          quiz={quiz} />)}</div>
+    </>
+  );
+}
+
+export default QuizCollection;
