41f78a3ef72b7ea777ebb23ef14dd48f46ed9aeb
karhukopla187 <ohmanpatrick@gmail.com>
2022-12-14 08:16:33 +0200

    a

--- testitestit/src/App.js
+++ testitestit/src/App.js
@@ -1,18 +1,49 @@
-import * as React from 'react';
-import { useState } from "react";
+import * as React from 'react'
+import { useState } from "react"
+import axios from 'axios'
+const URL = 'http://hn.algolia.com/api/v1/search'
 
 function App() {
-    const [showElement, setShowElement] = useState(false);
-    return (
-        <div className="App">
-            <div className="container">
-                <label>
-                    <input type="checkbox" data-testid="cbShowHide" checked="{showElement}" onChange={e => setShowElement(e.target.checked)} />
-                    <span>Show/Hide Box</span>
-                </label>
-                {showElement && <div className="box" data-testid="box">klikattu</div>}
-            </div>
-        </div>
-    );
+
+  const [stories, setStories] = React.useState([])
+  const [error, setError] = React.useState(null)
+
+  async function handleFetch(event) {
+    let result
+
+    try {
+      result = await axios.get(`${URL}?query=React`)
+
+      setStories(result.data.hits)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  const [showElement, setShowElement] = useState(false)
+  return (
+    <div className="App">
+      <div className="container">
+        <label>
+          <input type="checkbox" data-testid="cbShowHide" checked={showElement} onChange={e => setShowElement(e.target.checked)} />
+          <span>näytä/piilota</span>
+        </label>
+        {showElement && <div className="box" data-testid="box">klikattu</div>}
+      </div>
+      <button type="button" onClick={handleFetch}>
+        Hae juttuja
+      </button>
+
+      {error && <span>jotain meni pieleen</span>}
+
+      <ul>
+        {stories.map((story) => (
+          <li key={story.objectID}>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default App;
+export default App
--- testitestit/src/App.test.js
+++ testitestit/src/App.test.js
@@ -1,27 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
-import App from './App';
+import * as React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
+import axios from 'axios'
+import App from './App.js'
+
+jest.mock('axios')
 
 describe("<App />", () => {
+        test('axios mock', async () => {
+            const stories = [
+                { objectID: '1', title: 'tulos1' },
+                { objectID: '2', title: 'tulos2' },
+            ]
+            axios.get.mockImplementationOnce(() =>
+                Promise.resolve({ data: { hits: stories } })
+            )
+            render(<App />)
+            await userEvent.click(screen.getByRole('button'))
+            expect(items).toHaveLength(2)            
+            expect(screen.getAllByRole('listitem')).toHaveLength(2)
+        })
 
-    test('render component', () => {
-        render(<App />);
-        const checkbox = screen.getByTestId("cbShowHide");
-        expect(checkbox).toBeInTheDocument();
-        expect(checkbox).not.toBeChecked();
-        expect(screen.queryByTestId("box")).not.toBeInTheDocument();
-    });
-    test('toggle element by selecting checkbox', () => {
-        render(<App />);
-        const checkbox = screen.getByTestId("cbShowHide");
-        //klikkaus
-        userEvent.click(checkbox);
-        expect(checkbox).toBeChecked();
-        expect(screen.queryByTestId("box")).toBeInTheDocument();
-        //toinen klikkaus
-        userEvent.click(checkbox);
-        expect(checkbox).not.toBeChecked();
-        expect(screen.queryByTestId("box")).not.toBeInTheDocument();
-    });
+        test('render component', () => {
+            render(<App />);
+            const checkbox = screen.getByTestId("cbShowHide")
+            expect(checkbox).toBeInTheDocument()
+            expect(checkbox).not.toBeChecked()
+            expect(screen.queryByTestId("box")).not.toBeInTheDocument()
+        })
 
-});
+        test('toggle text by selecting checkbox', () => {
+            render(<App />)
+            const checkbox = screen.getByTestId("cbShowHide")
+            //klikkaus
+            userEvent.click(checkbox);
+            expect(checkbox).toBeChecked()
+            expect(screen.queryByTestId("box")).toBeInTheDocument()
+            //toinen klikkaus
+            userEvent.click(checkbox)
+            expect(checkbox).not.toBeChecked()
+            expect(screen.queryByTestId("box")).not.toBeInTheDocument()
+        })
+    })
